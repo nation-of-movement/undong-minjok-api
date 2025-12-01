da23ba2b550568509ee7a01945ee8d76582e5d89
zkvn1103@naver.com <zkvn1103@naver.com>
2025-12-01 18:12:56 +0900

    init

---
+++ undongminjok-api/src/main/java/com/undongminjok/api/UndongminjokApiApplication.java
@@ -0,0 +1,13 @@
+package com.undongminjok.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndongminjokApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndongminjokApiApplication.class, args);
+	}
+
+}
