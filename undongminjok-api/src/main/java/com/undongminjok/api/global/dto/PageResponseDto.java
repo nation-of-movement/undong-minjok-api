ce2ce5cbb329b20f426e1fc5a841fa97da40a129
sangdo19909-code <sangdo19909@gmail.com>
2025-12-05 21:38:28 +0900

    feature : global 페이지네이션 기능 추가
    공통 페이지네이션 기능 추가하였습니다.

---
+++ undongminjok-api/src/main/java/com/undongminjok/api/global/dto/PageResponseDto.java
@@ -0,0 +1,32 @@
+package com.undongminjok.api.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class PageResponseDto<T> {
+
+  private List<T> content;
+  private int page;
+  private int size;
+  private long totalElements;
+  private int totalPages;
+  private boolean first;
+  private boolean last;
+
+  public static <T> PageResponseDto<T> from(Page<T> page) {
+    return new PageResponseDto<>(
+        page.getContent(),
+        page.getNumber(),
+        page.getSize(),
+        page.getTotalElements(),
+        page.getTotalPages(),
+        page.isFirst(),
+        page.isLast()
+    );
+  }
+}
