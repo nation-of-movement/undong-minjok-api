97e5eabb79ee4cb3cb2a748b7f47ec8880c49796
zkvn99 <zkvn1103@naver.com>
2025-12-11 11:15:46 +0900

    fix : 임시 테스트 제거

--- undongminjok-api/src/test/java/com/undongminjok/api/UndongminjokApiApplicationTests.java
+++ undongminjok-api/src/test/java/com/undongminjok/api/UndongminjokApiApplicationTests.java
@@ -3,11 +3,11 @@ package com.undongminjok.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class UndongminjokApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class UndongminjokApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
