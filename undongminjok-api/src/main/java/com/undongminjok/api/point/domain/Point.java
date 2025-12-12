13fffadb1136661154a227840ef3299d6f309d07
lala19873@naver.com <lala19873@naver.com>
2025-12-12 12:21:09 +0900

    userId null -> not null

--- undongminjok-api/src/main/java/com/undongminjok/api/point/domain/Point.java
+++ undongminjok-api/src/main/java/com/undongminjok/api/point/domain/Point.java
@@ -33,7 +33,7 @@ public class Point extends BaseTimeEntity {
   private Long id;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "user_id")
+  @JoinColumn(name = "user_id", nullable = false)
   private User user;
 
   @ManyToOne(fetch = FetchType.LAZY)
